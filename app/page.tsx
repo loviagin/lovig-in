326afc6425d7f571ceb073343b95a64cba414329
Ilia Loviagin <ilia.loviagin@gmail.com>
2025-09-15 11:40:55 +0300

    Changed server port

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,18 @@
-// app/page.tsx
-function buildAuthorizeUrl() {
-  const u = new URL('http://localhost:4400/auth'); // сразу на провайдера!
-  u.searchParams.set('client_id', 'demo-web');
-  u.searchParams.set('redirect_uri', 'http://localhost:3300/api/oidc/cb'); // callback на Next (3300)
-  u.searchParams.set('response_type', 'code');
-  u.searchParams.set('scope', 'openid profile email');
-  u.searchParams.set('state', 'abc');
-  u.searchParams.set('nonce', 'def');
-  return u.toString();
-}
-
 export default function Home() {
-  const authorizeUrl = buildAuthorizeUrl();
+  const base = 'https://auth.lovig.in';
+  const redirect = `${base}/api/oidc/cb`;
+  const authorizeUrl =
+    `${base}/api/oidc/auth`
+    + `?client_id=demo-web`
+    + `&redirect_uri=${encodeURIComponent(redirect)}`
+    + `&response_type=code`
+    + `&scope=${encodeURIComponent('openid profile email')}`
+    + `&state=abc`
+    + `&nonce=def`;
+
   return (
     <main style={{ padding: 24 }}>
-      <a href={authorizeUrl} style={{ color: '#2563eb', textDecoration: 'underline' }}>
-        Войти (перейти на /auth провайдера)
-      </a>
+      <a href={authorizeUrl}>Войти</a>
     </main>
   );
 }
