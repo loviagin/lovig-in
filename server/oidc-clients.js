ee86a0092b569a0d1f2c93edfd5ac3308a9c77ad
Ilia Loviagin <ilia.loviagin@gmail.com>
2025-09-18 09:41:23 +0300

    Fixes demo client

--- server/oidc-clients.js
+++ server/oidc-clients.js
@@ -2,7 +2,7 @@ export default [
     {
         client_id: 'demo-web',
         client_name: 'Demo Web',
-        redirect_uris: ['https://auth.lovig.in'], // в dev
+        redirect_uris: ['https://auth.lovig.in/api/oidc/cb'], // в dev
         post_logout_redirect_uris: ['https://auth.lovig.in'],
         response_types: ['code'],
         grant_types: ['authorization_code', 'refresh_token'],
